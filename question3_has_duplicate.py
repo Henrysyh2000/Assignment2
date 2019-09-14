ec399b43c5d9ae248e52168ca5f3f7823bb943f9
Henrysyh2000 <48310939+Henrysyh2000@users.noreply.github.com>
2019-09-14 19:37:18 +0800

    Add files via upload

---
+++ question3_has_duplicate.py
@@ -0,0 +1,23 @@
+def has_duplicate(list1):
+    """
+    remember to mention your runtime as comment!
+
+    :param l: List -- list of integers
+    :return: True if list1 has duplicate, return False otherwise.
+    """
+    pass
+
+'''
+Note: 
+To get autograded on gradescope, you program can't print anything.
+
+Thus, please comment out the main function call, if you are submitting for auto grading.
+
+'''
+def main():
+    print(has_duplicate([0,6,2,4,9]))   # False
+
+    print(has_duplicate([0,6,2,4,9,1,2]))   # True
+
+if __name__ == '__main__':
+    main()
