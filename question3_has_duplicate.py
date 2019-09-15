b8910fe336489e00c9f15cecdcf13214e22e2b4e
Henrysyh2000 <15921942097@139.com>
2019-09-15 13:18:16 +0800

    Question 3

--- question3_has_duplicate.py
+++ question3_has_duplicate.py
@@ -1,11 +1,20 @@
 def has_duplicate(list1):
+# the Complexity of this fuction is O(n^2)
     """
     remember to mention your runtime as comment!
 
     :param l: List -- list of integers
     :return: True if list1 has duplicate, return False otherwise.
     """
-    pass
+    while len(list1) != 0:
+        new = list1[1:]
+        if list1[0] in new:
+            return True
+        else:
+            list1.pop(0)
+    return False
+        
+        
 
 '''
 Note: 
@@ -19,5 +28,5 @@ def main():
 
     print(has_duplicate([0,6,2,4,9,1,2]))   # True
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+    #main()
